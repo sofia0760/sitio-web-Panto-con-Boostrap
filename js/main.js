50b4c5085f9291e4d492ec1c52db7f73be27958f
sofia camelo <camelojimenezsofia20@gmail.com>
2025-09-10 17:00:06 -0500

    Finalice la seccion de productos y agregue la seccion de experiencias

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  
+    $('.slider-products').slick({
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+  
+});
+
+
+
+				
