74b6b7ffda7d0f07713c3b3f932eb2d83d6a8897
sofia camelo <camelojimenezsofia20@gmail.com>
2025-09-11 16:04:52 -0500

    Agregue  la seccion de testimonials

--- js/main.js
+++ js/main.js
@@ -37,8 +37,51 @@ $(document).ready(function(){
     // instead of a settings object
   ]
 });
+
+    $('.slider-reviews').slick({
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
   
 });
+
+
+
+
+
 
 
 
